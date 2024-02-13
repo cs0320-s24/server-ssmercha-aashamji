fda9382f1e000f8bd8d748b04ffa09d6eef635f6
akbarshamjijr <akbar_shamji@brown.edu>
2024-02-12 21:15:51 -0500

    feat: 1st implementation of server method code

--- src/main/java/edu/brown/cs/student/main/census/Server.java
+++ src/main/java/edu/brown/cs/student/main/census/Server.java
@@ -5,8 +5,28 @@ import spark.Response;
 import spark.Route;
 
 public class Server {
-    public static void main (String[] args){
         //this is the only method in server it should use Spark for the following: port, handlers, init, awaitInitialization
         //reference gear up code
-    }
+        public static void main(String[] args) {
+            int port = 3232;
+            Spark.port(port);
+
+            after(
+                    (request, response) -> {
+                        response.header("Access-Control-Allow-Origin", "*");
+                        response.header("Access-Control-Allow-Methods", "*");
+                    });
+
+            Spark.get("/", (req, res) -> "welcome to the census server.");
+
+            Spark.get("load", new loadHandler());
+            Spark.get("search", new searchHandler());
+            Spark.get("view", new viewHandler());
+
+            Spark.init();
+            Spark.awaitInitialization();
+
+            System.out.println("Server started at http://localhost:" + port);
+
+            }
 }
