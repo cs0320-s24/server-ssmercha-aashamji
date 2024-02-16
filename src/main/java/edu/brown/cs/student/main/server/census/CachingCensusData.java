8d3291ee0f430da96f1858ed4e8e7bc8a34ad427
akbarshamjijr <akbar_shamji@brown.edu>
2024-02-16 00:37:47 +0000

    feat: added guava to CachingCensusData

--- src/main/java/edu/brown/cs/student/main/server/census/CachingCensusData.java
+++ src/main/java/edu/brown/cs/student/main/server/census/CachingCensusData.java
@@ -1,17 +1,38 @@
 package edu.brown.cs.student.main.server.census;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
+import java.sql.Time;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class CachingCensusData implements CensusDataSource {
 
-    private final CensusDataSource original;
+  private final CensusDataSource original;
+  private final LoadingCache<String, CensusData> cache;
 
-    public CachingCensusData(CensusDataSource original){
-        this.original = original;
-    }
+  public CachingCensusData(CensusDataSource original) {
 
-    @Override
-    public CensusData getData() throws IllegalArgumentException {
-        return null;
+    this.original = original;
+    this.cache = CacheBuilder.newBuilder()
+            .maximumSize(100)
+            .expireAfterAccess(30, TimeUnit.MINUTES)
+            .build(new CacheLoader<String, CensusData>() {
+              @Override
+              public CensusData load(String s) throws Exception {
+                return original.getData();
+              }
+            });
+  }
+
+  @Override
+  public CensusData getData() throws IllegalArgumentException {
+    try{
+      return cache.get("constantKey");
+    } catch (ExecutionException e){
+      throw new RuntimeException("Error fetching data from cache.", e);
     }
+  }
 }
--- src/main/java/edu/brown/cs/student/main/server/census/CensusData.java
+++ src/main/java/edu/brown/cs/student/main/server/census/CensusData.java
@@ -1,4 +1,5 @@
 package edu.brown.cs.student.main.server.census;
+
 import java.util.List;
 
 public record CensusData(List<String> entry) {}
