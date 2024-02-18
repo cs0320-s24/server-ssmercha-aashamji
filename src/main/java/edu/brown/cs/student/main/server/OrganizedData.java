a422fd2040378fab329bf7338dd4c7f92ef36ed3
sanamerchant <123655714+sanamerchant@users.noreply.github.com>
2024-02-18 07:27:33 -0500

    chore: commenting

--- src/main/java/edu/brown/cs/student/main/server/OrganizedData.java
+++ src/main/java/edu/brown/cs/student/main/server/OrganizedData.java
@@ -5,27 +5,51 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This is our proxy class for shared state information between the handlers, it is passed in to each handler
+ * in Server.java
+ */
 public class OrganizedData {
   public List results;
   public Parser myParser;
   public Map<String, Boolean> pathLoaded = new HashMap<>();
 
+  /**
+   * This is our constructor
+   */
   public OrganizedData() {
     results = new ArrayList<>();
   }
 
+  /**
+   * This updates results to a copy of a list
+   * @param given is the list to make a copy of
+   */
   public void updateList(List given) {
     this.results = List.copyOf(given);
   }
 
+  /**
+   * This sets the parser field to a given parser
+   * @param p the parser to reference
+   */
   public void parserRef(Parser p) {
     myParser = p;
   }
 
+  /**
+   * This updates a state in the pathLoaded map, meant to tore whether file has been loaded
+   * @param filepath is the path of the file to set the status of
+   */
   public void updateState(String filepath) {
     pathLoaded.put(filepath, true);
   }
 
+  /**
+   * This returns a boolean representing whether a file has been loaded
+   * @param filepath is the file whose load status we're checking
+   * @return
+   */
   public boolean isLoaded(String filepath) {
     if (pathLoaded.containsKey(filepath)) {
       if (pathLoaded.get(filepath)) {
--- src/main/java/edu/brown/cs/student/main/server/census/broadbandHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/census/broadbandHandler.java
@@ -20,10 +20,20 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+/**
+ * This class implements our handler for Census API requests
+ */
 public class broadbandHandler implements Route {
   Map<String, Object> responseMap = new HashMap<>();
   CachingCensusData cache;
 
+  /**
+   * This is our handle method which interacts with the API to return the wanted information
+   * @param request the request object providing information about the HTTP request
+   * @param response the response object providing functionality for modifying the response
+   * @return a serialized json of the responseMap
+   * @throws Exception
+   */
   @Override
   public Object handle(Request request, Response response) throws Exception {
     String state = request.queryParams("state");
@@ -37,7 +47,6 @@ public class broadbandHandler implements Route {
     JsonAdapter<Map<String, Object>> adapter = moshi.adapter(mapStringObject);
 
     if("true".equalsIgnoreCase(useCache)){
-
       CensusData cachedData = cache.getData(query);
       if(cachedData == null){
         return adapter.toJson(cachedData);
@@ -46,35 +55,54 @@ public class broadbandHandler implements Route {
       //check
     }
 
-    String states = this.sendStateCodesRequest();
+    String states = this.sendStateCodesRequest(); //to get a String representing all states and their codes
 
+    //deserialize into a list
     List<List<String>> stateCodesList = CensusAPIUtilities.deserialize(states);
+    //convert list to map
     Map<String, String> stateCodesMap = CensusAPIUtilities.listToMap(stateCodesList, 0);
 
+    //get the state code for the specific state
     String sCode = stateCodesMap.get(state);
+    //get all county codes
     String counties = this.sendCountyCodesRequest(sCode);
-    try {
-      List<List<String>> countyCodesList = CensusAPIUtilities.deserialize(counties);
-      Map<String, String> countyCodesMap = CensusAPIUtilities.listToMap(countyCodesList, 1);
 
+    try {
+      //deserialize county codes into list
+      List<List<String>> countyCodesList = CensusAPIUtilities.deserialize(counties);
+      //convert list to string
+      Map<String, String> countyCodesMap = CensusAPIUtilities.listToMap(countyCodesList, 1);
+      //get specific county code
       String countyCode = countyCodesMap.get(county + ", " + state);
 
+      //make broadband request
       String broadBandPercentage = this.sendBroadBandRequest(sCode, countyCode);
+      //deserialize broadband request into list
       List<List<String>> broadBandList = CensusAPIUtilities.deserialize(broadBandPercentage);
 
+      //enter results into responseMap
       responseMap.put("state", state);
       responseMap.put("county", county);
       responseMap.put("broadband Access Percentage", broadBandList.get(1).get(1));
       responseMap.put("result", "success");
-    } catch (EOFException e) {
+    } catch (EOFException e) { //missing any input
       responseMap.put("result", "error_bad_request");
     }
 
-    return adapter.toJson(responseMap);
+    return adapter.toJson(responseMap); //return json
   }
 
+  /**
+   * This sends a request to get the broadband percentage given a state code and county code
+   * @param sCode state code
+   * @param countyCode county code
+   * @return percentage
+   * @throws IOException for send()
+   * @throws InterruptedException for send()
+   * @throws URISyntaxException for uri()
+   */
   private String sendBroadBandRequest(String sCode, String countyCode)
-      throws IOException, InterruptedException, URISyntaxException {
+          throws IOException, URISyntaxException, InterruptedException {
     HttpRequest buildBoredApiRequest =
         HttpRequest.newBuilder()
             .uri(
@@ -98,6 +126,14 @@ public class broadbandHandler implements Route {
     return sentBoredApiResponse.body();
   }
 
+  /**
+   * This sends a request to get the broadband percentage given a state code and county code
+   * @param state state name
+   * @return state code
+   * @throws IOException for send()
+   * @throws InterruptedException for send()
+   * @throws URISyntaxException for uri()
+   */
   private String sendCountyCodesRequest(String state)
       throws URISyntaxException, IOException, InterruptedException {
     HttpRequest buildBoredApiRequest =
@@ -121,7 +157,14 @@ public class broadbandHandler implements Route {
 
     return sentBoredApiResponse.body();
   }
-
+  
+  /**
+   * This sends a request to get the broadband percentage given a state code and county code
+   * @return all states and their codes
+   * @throws IOException for send()
+   * @throws InterruptedException for send()
+   * @throws URISyntaxException for uri()
+   */
   private String sendStateCodesRequest()
       throws URISyntaxException, IOException, InterruptedException {
     HttpRequest buildBoredApiRequest =
